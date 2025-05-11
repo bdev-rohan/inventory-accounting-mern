a5cb16340c6a17c50af8ca05acb3a672f7cf732f
R.Basudev <bdevrohan@gmail.com>
2025-05-11 11:42:40 +0530

    changed some ui colors

--- client/src/components/layout/Sidebar.tsx
+++ client/src/components/layout/Sidebar.tsx
@@ -1,12 +1,12 @@
-import {useState} from 'react';
-import {Outlet, useNavigate} from 'react-router-dom';
-import {Button, Layout, Menu} from 'antd';
-import {LogoutOutlined} from '@ant-design/icons';
-import {sidebarItems} from '../../constant/sidebarItems';
-import {useAppDispatch} from '../../redux/hooks';
-import {logoutUser} from '../../redux/services/authSlice';
+import { useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { Button, Layout, Menu } from "antd";
+import { LogoutOutlined } from "@ant-design/icons";
+import { sidebarItems } from "../../constant/sidebarItems";
+import { useAppDispatch } from "../../redux/hooks";
+import { logoutUser } from "../../redux/services/authSlice";
 
-const {Content, Sider} = Layout;
+const { Content, Sider } = Layout;
 
 const Sidebar = () => {
   const [showLogoutBtn, setShowLogoutBtn] = useState(true);
@@ -15,60 +15,67 @@ const Sidebar = () => {
 
   const handleClick = () => {
     dispatch(logoutUser());
-    navigate('/');
+    navigate("/");
   };
 
   return (
-    <Layout style={{height: '100vh'}}>
+    <Layout style={{ height: "100vh" }}>
       <Sider
-        breakpoint='lg'
-        collapsedWidth='0'
+        breakpoint="lg"
+        collapsedWidth="0"
         onCollapse={(collapsed, type) => {
-          if (type === 'responsive') {
+          if (type === "responsive") {
             setShowLogoutBtn(!collapsed);
           }
-          if (type === 'clickTrigger') {
+          if (type === "clickTrigger") {
             setShowLogoutBtn(!collapsed);
           }
         }}
-        width='220px'
+        width="220px"
         style={{
-          backgroundColor: '#164863',
-          position: 'relative',
+          backgroundColor: "#8ad669",
+          position: "relative",
         }}
       >
-        <div className='demo-logo-vertical'>
-          <h1 style={{color: '#fff', padding: '1rem', fontSize: '1.8rem', textAlign: 'center'}}>
+        <div className="demo-logo-vertical">
+          <h1
+            style={{
+              color: "#fff",
+              padding: "1rem",
+              fontSize: "1.8rem",
+              textAlign: "center",
+            }}
+          >
             WELCOME
           </h1>
         </div>
         <Menu
-          theme='dark'
-          mode='inline'
-          style={{backgroundColor: '#164863', fontWeight: '700'}}
-          defaultSelectedKeys={['Dashboard']}
+          theme="dark"
+          mode="inline"
+          style={{ backgroundColor: "#8ad669", fontWeight: "700" }}
+          defaultSelectedKeys={["Dashboard"]}
           items={sidebarItems}
         />
         {showLogoutBtn && (
           <div
             style={{
-              margin: 'auto',
-              position: 'absolute',
+              margin: "auto",
+              position: "absolute",
               bottom: 0,
-              padding: '1rem',
-              display: 'flex',
-              width: '100%',
-              justifyContent: 'center',
+              padding: "1rem",
+              display: "flex",
+              width: "100%",
+              justifyContent: "center",
             }}
           >
             <Button
-              type='primary'
+              type="primary"
               style={{
-                width: '100%',
-                backgroundColor: 'cyan',
-                color: '#000',
+                width: "100%",
+                backgroundColor: "cyan",
+                color: "#000",
                 fontWeight: 600,
-                textTransform: 'uppercase',
+                textTransform: "uppercase",
               }}
               onClick={handleClick}
             >
@@ -79,15 +86,15 @@ const Sidebar = () => {
         )}
       </Sider>
       <Layout>
-        <Content style={{padding: '2rem', background: '#BBE1FA'}}>
+        <Content style={{ padding: "2rem", background: "#BBE1FA" }}>
           <div
             style={{
-              padding: '1rem',
-              maxHeight: 'calc(100vh - 4rem)',
-              minHeight: 'calc(100vh - 4rem)',
-              background: '#fff',
-              borderRadius: '1rem',
-              overflow: 'auto',
+              padding: "1rem",
+              maxHeight: "calc(100vh - 4rem)",
+              minHeight: "calc(100vh - 4rem)",
+              background: "#fff",
+              borderRadius: "1rem",
+              overflow: "auto",
             }}
           >
             <Outlet />
